d1fc8ca1180d0698bd9833af1ae41045b5bfd278
anthonymruiz <anthonymruiz@hotmail.com>
2022-03-02 10:37:46 -0400

    Function to calculate coins

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-//1. Function to print N fibbonacci
+(function() {
+    console.log("Fibonaccis");
+    console.log(getFibo(10));
 
+    console.log("Random and uniques numbers from 1 to random number");
+    console.log(randomOrder(10));
+
+    console.log("Obtener cambio");
+    console.log(getCoins(17984));
+})();
+
+//1. Function to print N fibbonacci
 function getFibo(n){ 
     let next = 1;
     let previous = 0;  
@@ -12,8 +22,7 @@ function getFibo(n){
        numbers.push(next);
     }
     return numbers;
-}
-getFibo(10); 
+} 
 
 //2. Function to random Order 
 function randomOrder(n){
@@ -23,5 +32,19 @@ function randomOrder(n){
     } 
     randoms = randoms.sort(r =>  Math.random() - 0.5);
     return randoms; 
+}  
+
+//3. Funciton 
+function getCoins(amount){
+    let coins = [2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1].sort((a,b) => b-a); 
+    let output  = [];
+    coins.forEach(coin => {
+       if(coin < amount){
+          let cantidad = Math.floor(amount / coin);
+          let total = cantidad * coin; 
+          output.push(`${cantidad} x ${coin} = ${total}`); 
+          amount -= total;
+       } 
+    }) 
+    return output;
 } 
-randomOrder(10);
